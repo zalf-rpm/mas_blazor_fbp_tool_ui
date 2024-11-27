4655ff9ba9a9ac12ba714f9d10927e4c4bf86d72
Michael Berg-Mohnicke <michael.berg@zalf.de>
2024-11-27 18:24:16 +0100

    initial commit after move from mas-infrastructure

---
+++ Models/CapnpFbpIipModel.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorDrawFBP.Models;
+
+using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+public class CapnpFbpIipModel : NodeModel
+{
+    public CapnpFbpIipModel(Point position = null) : base(position) {}
+    
+    public string ComponentId { get; set; }
+    
+    public string Content { get; set; }
+}
